70c46c26197e852de40cc1102ddc58de27fd762f
Vin Saceda <vinsaceda02@gmail.com>
2020-07-20 01:33:16 +0800

    User Authentication

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 const userController = require('../controllers/UserController')
+var jwtAuthMiddleware = require("./../middlewares/JwtAuthMiddleware")
 
 module.exports = router => {
-    router.route('/user').post(userController.addUser)
-    router.route('/user/:id').get(userController.getUser)
-    router.route('/users').get(userController.getAllUsers)
+    router.route('/user').post(jwtAuthMiddleware, userController.addUser)
+    router.route('/user/:id').get(jwtAuthMiddleware, userController.getUser)
+    router.route('/users').get(jwtAuthMiddleware, userController.getAllUsers)
 }
